00f046f2522e8e2f8139968186e16b401294ffdc
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2020-04-18 01:49:40 -0400

    countDown function adjusted to deal with end of countdown.

--- js/main.js
+++ js/main.js
@@ -12,26 +12,39 @@ function countDown(){
     let launchTime = launchDate.getTime();
     let dateDifference = launchTime - currentTime;
 
-    let diffSeconds = Math.floor(dateDifference / 1000); //milliseconds to seconds
-    let diffMinutes = Math.floor(diffSeconds / 60) ;
-    let diffHours = Math.floor(diffMinutes / 60);
-    let diffDays = Math.floor(diffHours / 24);
+    let diffDays;
+    let diffHours;
+    let diffMinutes;
+    let diffSeconds;
 
-    diffSeconds = diffSeconds % 60;
-    diffMinutes = diffMinutes % 60;
-    diffHours = diffHours % 24;
+    if (dateDifference < 0) {
+        clearTimeout(timer);
+        days.textContent = "0";
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+    } 
+    else {
+        diffSeconds = Math.floor(dateDifference / 1000); //milliseconds to seconds
+        diffMinutes = Math.floor(diffSeconds / 60) ;
+        diffHours = Math.floor(diffMinutes / 60);
+        diffDays = Math.floor(diffHours / 24);
 
-    diffSeconds = (diffSeconds < 10) ? "0" + diffSeconds : diffSeconds;
-    diffMinutes = (diffMinutes < 10) ? "0" + diffMinutes : diffMinutes;
-    diffHours = (diffHours < 10) ? "0" + diffHours : diffHours;
+        diffSeconds = diffSeconds % 60;
+        diffMinutes = diffMinutes % 60;
+        diffHours = diffHours % 24;
 
+        diffSeconds = (diffSeconds < 10) ? "0" + diffSeconds : diffSeconds;
+        diffMinutes = (diffMinutes < 10) ? "0" + diffMinutes : diffMinutes;
+        diffHours = (diffHours < 10) ? "0" + diffHours : diffHours;
 
-    days.textContent = diffDays;
-    hours.textContent = diffHours;
-    minutes.textContent = diffMinutes;
-    seconds.textContent = diffSeconds;
+        days.textContent = diffDays;
+        hours.textContent = diffHours;
+        minutes.textContent = diffMinutes;
+        seconds.textContent = diffSeconds;
 
-    setTimeout(countDown, 1000);
+        let timer = setTimeout(countDown, 1000);
+    }
 }
 
 countDown();
